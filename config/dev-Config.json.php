04506c21967bcd9ee7fb9e4fd059fac194c722a2
Morris Jobke <morris.jobke@gmail.com>
2012-06-09 18:35:13 +0200

    updated config and submodule revision

--- config/dev-Config.json.php
+++ config/dev-Config.json.php
@@ -2,4 +2,5 @@
 	"homePath": 						"/home/urz/<?php echo substr($_SERVER['REMOTE_USER'], 0, 1).'/'.$_SERVER['REMOTE_USER'] ?>",
 	"widget.treeMenu.excludeFolder":	"^\/home\/urz(\/[^\/]+)?$",
 	"widget.treeMenu.rootNodeName": 	"/afs/tu-chemnitz.de",
+	"statusbar.left": 					["quota.progress"],
 }
